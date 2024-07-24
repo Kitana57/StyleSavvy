48b449407990646c2cdce56819a5bea2a06bf368
Kitana57 <sonalitripathy655@gmail.com>
2024-07-24 19:44:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("login-modal");
+    const btn = document.getElementById("sign-in-btn");
+    const span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
